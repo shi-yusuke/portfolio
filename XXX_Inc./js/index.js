0925473dd6a37706259bc850aae842d982042024
shi-yusuke <117758926+shi-yusuke@users.noreply.github.com>
2022-12-13 04:04:20 +0900

    add XXX_Inc.

---
+++ XXX_Inc./js/index.js
@@ -0,0 +1,21 @@
+// トリガー アニメーション処理
+$(window).ready(function () {
+  var scrollAnimationElm = document.querySelectorAll('.scroll_up,.scroll_left,.scroll_right,.mv');
+	console.log(scrollAnimationElm);
+  var scrollAnimationFunc = function () {
+    for (var i = 0; i < scrollAnimationElm.length; i++) {
+      var triggerMargin = 75;
+      if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+        scrollAnimationElm[i].classList.add('on');
+      }
+    }
+  }
+  window.addEventListener('load', scrollAnimationFunc);
+  window.addEventListener('scroll', scrollAnimationFunc);
+});
+
+// ハンバーガーボタン
+$(document).on('click', '.hamburger', function(){
+$(this).toggleClass('active');
+$('.header-nav__list').toggleClass('active');
+});
